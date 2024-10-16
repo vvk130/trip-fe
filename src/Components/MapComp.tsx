23bd90fee02cea59c0e61893be6b9701ec0b38ff
vvk130 <vanessa.kasula@outlook.com>
2024-10-16 20:52:07 +0300

    Map will go over all coordinates on page

--- src/Components/MapComp.tsx
+++ src/Components/MapComp.tsx
@@ -3,20 +3,37 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
 import "leaflet-defaulticon-compatibility";
 import iconMarker from "leaflet/dist/images/marker-icon.png";
-import { useState } from "react";
 import ResponsivePagination from "react-responsive-pagination";
 import "react-responsive-pagination/themes/classic.css";
 import Table from "../Utils/Table";
 import SearchBar from "./Forms/SearchBar";
 import L from "leaflet";
-
-const defaultCenter: [number, number] = [51.505, -0.09];
-const defaultZoom: number = 8;
+import { useState } from "react";
 
 const MapComp = (): JSX.Element => {
   const [currentPage, setCurrentPage] = useState(8);
   const totalPages = 20;
 
+  const positions = [
+    { coordinateX: null, coordinateY: null },
+    { coordinateX: 51.51, coordinateY: -0.1 },
+    { coordinateX: 51.52, coordinateY: -0.12 },
+    { coordinateX: 51.53, coordinateY: -0.08 },
+    { coordinateX: 51.54, coordinateY: -0.07 },
+    { coordinateX: 51.55, coordinateY: -0.06 },
+    { coordinateX: 51.56, coordinateY: -0.11 },
+    { coordinateX: 55.57, coordinateY: -0.1 },
+    { coordinateX: 51.58, coordinateY: -0.09 },
+    { coordinateX: 50.59, coordinateY: -0.08 },
+  ];
+
+  const defaultCenter: [number, number] =
+    positions?.length > 0 &&
+    positions[0].coordinateX != null &&
+    positions[0].coordinateY != null
+      ? [positions[0].coordinateX, positions[0].coordinateY]
+      : [60.1699, 24.9384];
+  const defaultZoom: number = 12;
   const icon = L.icon({ iconUrl: iconMarker });
 
   const data = [
@@ -55,11 +72,22 @@ const MapComp = (): JSX.Element => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]} icon={icon}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {positions?.map((position) => {
+          if (position.coordinateX != null && position.coordinateY != null) {
+            return (
+              <Marker
+                key={
+                  position.coordinateX.toString() +
+                  position.coordinateY.toString()
+                }
+                position={[position.coordinateX, position.coordinateY]}
+                icon={icon}
+              >
+                <Popup>Station</Popup>
+              </Marker>
+            );
+          }
+        })}
       </MapContainer>
     </>
   );
